23985bdc337b78b96b462045063afe66cfbd7e1e
Zenna Tavares <zennatavares@gmail.com>
2016-09-23 19:09:58 -0400

    updating resnet

--- tensortemplates/res_net.py
+++ tensortemplates/res_net.py
@@ -1,14 +1,25 @@
 import tensorflow as tf
-import pi.util
 import numpy as np
+
+def same(xs):
+    """All elements in xs are the same"""
+    if len(xs) == 0:
+        return True
+    else:
+        x1 = xs[0]
+        for xn in xs:
+            if xn != x1:
+                return False
+
+    return True
 
 def consistent_batch_size(shapes):
     """Are the batch sizes the same"""
-    return pi.util.same([shape[0] for shape in shapes])
+    return same([shape[0] for shape in shapes])
 
-def batch_flatten(tensors):
-    """Flatten a vector of tensors, checking batch sizes are same"""
-    return [tf.reshape(t,[t.get_shape()[0].value,-1]) for t in tensors]
+def batch_flatten(t):
+    """Reshape tensor so that it is flat vector (except for batch dimension)"""
+    return tf.reshape(t, [tf.shape(t)[0], -1])
 
 
 def res_net_template_dict(inputs, out_shapes, **kwargs):
@@ -17,20 +28,23 @@ def res_net_template_dict(inputs, out_shapes, **kwargs):
     outputs, params = res_net_template(input_list, out_shapes_list, **kwargs)
     return dict(zip(out_shapes.keys(), outputs)), params
 
-
-def res_net(inputs, out_shapes, **kwargs):
+def res_net(inputs, inp_shapes, out_shapes, **kwargs):
     """
     Residual neural network inputs
     inputs : [tf.Tensor/tf.Variable] - inputs to by transformed
     out_shapes : (tf.TensorShape) | (Int) - shapes of output of tensor (includes batch_size)
     """
-    # layer_width = kwargs['layer_width']
-    # nblocks = kwargs['nblocks']
-    # block_size = kwargs['block_size']
-    # output_args = kwargs['output_args']
-    inp_shapes = [x.get_shape().as_list() for x in inputs]
+    ## Meta Parameters
+    layer_width = kwargs['layer_width']
+    nblocks = kwargs['nblocks']
+    block_size = kwargs['block_size']
+    output_args = kwargs['output_args']
+
+    ## Handle Reshaping (res_net expects input as vector)
+    # inp_shapes = [x.get_shape().as_list() for x in inputs]
     print("Input Shapes to Resnet", inp_shapes)
     print("Output Shapes to Resnet", out_shapes)
+    # import pdb; pdb.set_trace()
     assert consistent_batch_size(inp_shapes + out_shapes), "Batch sizes differ"
     flat_input_shapes = [np.prod(inp_shape[1:]) for inp_shape in inp_shapes]
     input_width = np.sum(flat_input_shapes)
@@ -44,13 +58,12 @@ def res_net(inputs, out_shapes, **kwargs):
 
     params = []
 
-    flat_inputs = batch_flatten(inputs)
+    flat_inputs = [batch_flatten(inp) for inp in inputs]
     x = tf.concat(1, flat_inputs)
 
     ## Layers
-    W = tf.Variable(tf.random_uniform([input_width, output_width]), name="W")
-    # W = tf.Variable(tf.zeros([input_width, output_width]), name="W")
-    b = tf.Variable(tf.zeros([output_width]), name="b")
+    W = tf.get_variable(name="W", shape=(input_width, output_width), initializer=tf.random_uniform)
+    b = tf.get_variable(name="b", shape=(output_width), initializer=tf.zeros)
     l1_op = tf.matmul(x, W) + b
     params = params + [W, b]
 
@@ -61,8 +74,7 @@ def res_net(inputs, out_shapes, **kwargs):
     for i in range(noutputs):
         ub = lb + flat_output_shapes[i]
         out = output_product[:, lb:ub]
-        new_shape = [out.get_shape().as_list()[0]] + out_shapes[i][1:]
-        print("newshape", new_shape)
+        new_shape = tf.shape(out)[0] + out_shapes[i][1:]
         rout = tf.reshape(out, new_shape)
         outputs.append(rout)
         lb = ub
